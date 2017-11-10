f2ccff0e07e6f007cc8fcfd8bd284b967b2cf9ec
Mio <a771644612@live.com>
2017-11-10 11:12:26 +0800

    修改了sendMsg的实现

--- src/cqjsdk/server/Sender.java
+++ src/cqjsdk/server/Sender.java
@@ -9,25 +9,29 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class Sender extends Thread{
-    private static  Sender sender = new Sender();
+    private static Sender sender = new Sender();
     private static BlockingQueue<Msg> sendq;
     private static Integer target_port;
     private static DatagramSocket socket;
     private Sender(){ }
 
     public static Sender getSender(DatagramSocket server, Integer target_port){
-        try {
-            Sender.target_port = target_port;
-            Sender.socket = server;
-        }
-        catch (Exception ex){
-            ex.printStackTrace();
-        }
+        Sender.target_port = target_port;
+        Sender.socket = server;
         Sender.sendq = new ArrayBlockingQueue<Msg>(4096);
         return sender;
     }
 
     public static void sendMsg(Msg msg){
+        try {
+            sendq.put(msg);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private void sendMsg_imp(Msg msg){
         try {
             byte[] buf = msg.encode();
             InetAddress ip = InetAddress.getLoopbackAddress();
@@ -44,7 +48,7 @@ public class Sender extends Thread{
         try{
             while(true){
                 msg = sendq.take();
-                sendMsg(msg);
+                sendMsg_imp(msg);
             }
         }
         catch (Exception ex){
