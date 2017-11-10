eac57f3da179a187c974fd9e8462fdc0a04e54fd
Mio <a771644612@live.com>
2017-11-10 11:39:17 +0800

    添加了两个便于分离Image和At的pattern

--- src/cqjsdk/CQJModule.java
+++ src/cqjsdk/CQJModule.java
@@ -7,12 +7,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 abstract public class CQJModule {
 
     private static ArrayList<CQJModule> modulelist = new ArrayList<CQJModule>();
     protected Set<String> toget;
     protected Boolean started;
+    protected Pattern CQIMG_PATTERN = Pattern.compile("\\[CQ:image,file=(.+?)\\]");
+    protected Pattern CQAT_PATTERN = Pattern.compile("\\[CQ:at,qq=(\\d+?)\\]");
 
     protected CQJModule(){
         started = true; // TODO:改成收到ServerHello再started
